25456703168369e85f465bab95d67bee0fcf1735
Your Name <you@example.com>
2025-10-19 17:53:47 +0700

    Update Category model & application.properties

--- src/main/java/com/example/tuevents/model/Category.java
+++ src/main/java/com/example/tuevents/model/Category.java
@@ -1,9 +1,11 @@
 package com.example.tuevents.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "category")
+@NoArgsConstructor  
 public class Category {
 
     @Id
