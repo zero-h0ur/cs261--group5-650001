c67c02f96edb63c33f3f2b93166d8e1710003bd7
Micosz <Chanakarn.kon@dome.tu.ac.th>
2025-11-04 00:52:46 +0700

    Add Camel.JSON&SQL

---
+++ SQL/Camel/category_table.sql
@@ -0,0 +1,20 @@
+DELETE FROM dbo.[event];
+GO
+
+DELETE FROM dbo.[category];
+GO
+
+SELECT * FROM dbo.[category];
+
+DBCC CHECKIDENT ('dbo.[category]', RESEED, 0);
+GO
+
+----------------------------------------------------
+
+INSERT INTO dbo.[category] (categoryName)
+VALUES (N'Camp'), (N'Activity'), (N'Music'), (N'Technology'), (N'Sport'),
+       (N'Competition'), (N'Workshop'), (N'Art'), (N'Academic'),
+       (N'Finance'), (N'Business');
+GO
+
+SELECT * FROM dbo.[category] ORDER BY categoryId;
